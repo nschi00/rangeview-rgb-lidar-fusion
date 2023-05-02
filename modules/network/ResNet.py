20bfb1071eb8bfa4a295b9d44f37fe6cedb0e11a
nschi00 <131670912+nschi00@users.noreply.github.com>
2023-05-02 11:53:26 +0200

    First try of fusion concatentation with pad & conv

--- modules/network/ResNet.py
+++ modules/network/ResNet.py
@@ -5,11 +5,12 @@ import torch.nn as nn
 import torch
 from torch.nn import functional as F
 import numpy as np
-from torchvision.models.segmentation import fcn_resnet50, FCN_ResNet50_Weights, deeplabv3_resnet50
-from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2
+# from torchvision.models.segmentation import fcn_resnet50, FCN_ResNet50_Weights, deeplabv3_resnet50
+# from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2
 from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
+from transformers import AutoImageProcessor, Mask2FormerForUniversalSegmentation
 
-from third_party.SwinFusion.models.network_swinfusion import SwinFusion as net
+# from third_party.SwinFusion.models.network_swinfusion import SwinFusion as net
 from torchvision.transforms.transforms import Resize
 
 
@@ -197,40 +198,165 @@ class ResNet_34(nn.Module):
             return [out, res_2, res_3, res_4]
         else:
             return out
-        
+
 class Fusion(nn.Module):
-    def __init__(self,nclasses, aux = True) -> None:
-        super().__init__()
-        self.num_classes = nclasses
+    def __init__(self, nclasses, aux=False, block=BasicBlock, layers=[3, 4, 6, 3], if_BN=True, zero_init_residual=False,
+                 norm_layer=None, groups=1, width_per_group=64):
+        super(Fusion, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+        self.if_BN = if_BN
+        self.dilation = 1
         self.aux = aux
-        self.backbone  = resnet_fpn_backbone('resnet50', weights='ResNet50_Weights.IMAGENET1K_V2', trainable_layers=5) 
-        self.feature_extract = FeatureExtractionBlock()
-        self.init_conv = BasicConv2d(5, 3, kernel_size=3, padding=1)
-        self.feature_reduction_1 =BasicConv2d(256, 128, kernel_size=1, padding=0)
-        self.feature_reduction_2 =BasicConv2d(128, 64, kernel_size=1, padding=0)
-        self.predict_com =BasicConv2d(64*4, self.num_classes, kernel_size=1, padding=0)
-        self.predict =BasicConv2d(64, self.num_classes, kernel_size=1, padding=0)
+
+        self.groups = groups
+        self.base_width = width_per_group
+
+        self.conv1 = BasicConv2d(5, 64, kernel_size=3, padding=1)
+        self.conv2 = BasicConv2d(64, 128, kernel_size=3, padding=1)
+        self.conv3 = BasicConv2d(128, 128, kernel_size=3, padding=1)
+
+        self.inplanes = 128
+
+        self.layer1 = self._make_layer(block, 128, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 128, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 128, layers[3], stride=2)
+
+        self.conv_1 = BasicConv2d(640, 256, kernel_size=3, padding=1)
+        self.conv_2 = BasicConv2d(256, 128, kernel_size=3, padding=1)
+        self.semantic_output = nn.Conv2d(128, nclasses, 1)
+
+        if self.aux:
+            self.aux_head1 = nn.Conv2d(128, nclasses, 1)
+            self.aux_head2 = nn.Conv2d(128, nclasses, 1)
+            self.aux_head3 = nn.Conv2d(128, nclasses, 1)
+
+        self.processor = AutoImageProcessor.from_pretrained("facebook/mask2former-swin-tiny-cityscapes-panoptic")
+        self.mask2former = Mask2FormerForUniversalSegmentation.from_pretrained("facebook/mask2former-swin-tiny-cityscapes-panoptic")
+
+        self.pool_mask = nn.MaxPool2d(kernel_size=(2,1), stride=(2,1))
+        self.conv_fusion1 = BasicConv2d(384, 128, kernel_size=3, padding=1)
+        self.conv_fusion2 = BasicConv2d(384, 128, kernel_size=3, padding=1)
+        self.conv_fusion3 = BasicConv2d(384, 128, kernel_size=3, padding=1)
+        self.conv_fusion4 = BasicConv2d(384, 128, kernel_size=3, padding=1)
+
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            if self.if_BN:
+                downsample = nn.Sequential(
+                    conv1x1(self.inplanes, planes * block.expansion, stride),
+                    norm_layer(planes * block.expansion),
+                )
+            else:
+                downsample = nn.Sequential(
+                    conv1x1(self.inplanes, planes * block.expansion, stride)
+                )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, if_BN=self.if_BN))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                if_BN=self.if_BN))
+
+        return nn.Sequential(*layers)
 
     def forward(self, x, rgb):
-        x_feature = self.feature_extract(x)
-        x = self.init_conv(x)
-        x = self.backbone(x)
-        x.pop("pool")
-        x.pop("3")
-        x = list(x.values())
-        x = [x_feature] + x
-        for i, feature in enumerate(x):
-            x[i] = self.feature_reduction_2(self.feature_reduction_1(F.interpolate(feature, size=x_feature.size()[2:], mode='bilinear', align_corners=True)))
+        rgb_input = self.processor(images=list(rgb), return_tensors="pt").to(self.device)
+        with torch.no_grad():
+            mask2former_results = self.mask2former(**rgb_input, output_hidden_states=True)
+            ## outputs feature maps of width/height 12x12, 24x24, 48x48, (bs, 256, 96, 96)
 
-        final_predict = self.predict_com(torch.cat(x, dim=1))
-        final_predict = F.softmax(final_predict, dim=1)
+        m =  self.pool_mask(mask2former_results['pixel_decoder_last_hidden_state'])
+        m = F.pad(m, (208, 208, 8, 8), "constant", 0)
+        m_1 = F.pad(mask2former_results['pixel_decoder_hidden_states'][2], (232, 232, 8, 8),  "constant", 0)
+        m_2 = F.pad(mask2former_results['pixel_decoder_hidden_states'][1], (116, 116, 4, 4),  "constant", 0)
+        m_3 = F.pad(mask2former_results['pixel_decoder_hidden_states'][0], (58, 58, 2, 2), "constant", 0)
+
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)    # (bs, 128, 64, 512)
+        x = self.conv_fusion1(torch.cat((x, m), dim=1))
+
+        x_1 = self.layer1(x)  # 1      (bs, 128, 64, 512)
+        x_1 = self.conv_fusion2(torch.cat((x_1, m_1), dim=1))
+        x_2 = self.layer2(x_1)  # 1/2  (bs, 128, 32, 256)
+        x_2 = self.conv_fusion3(torch.cat((x_2, m_2), dim=1))
+        x_3 = self.layer3(x_2)  # 1/4  (bs, 128, 16, 128)
+        x_3 = self.conv_fusion4(torch.cat((x_3, m_3), dim=1))
+        x_4 = self.layer4(x_3)  # 1/8  (bs, 128, 8, 64)
+
+        res_2 = F.interpolate(x_2, size=x.size()[2:], mode='bilinear', align_corners=True)
+        res_3 = F.interpolate(x_3, size=x.size()[2:], mode='bilinear', align_corners=True)
+        res_4 = F.interpolate(x_4, size=x.size()[2:], mode='bilinear', align_corners=True)
+        res = [x, x_1, res_2, res_3, res_4]
+
+        out = torch.cat(res, dim=1)
+        out = self.conv_1(out)
+        out = self.conv_2(out)
+        out = self.semantic_output(out)
+        out = F.softmax(out, dim=1)
+
         if self.aux:
-            predict_1 = F.softmax(self.predict(x[1]), dim=1)
-            predict_2 = F.softmax(self.predict(x[2]), dim=1)
-            predict_3 = F.softmax(self.predict(x[3]), dim=1)
-            return [final_predict, predict_1, predict_2, predict_3]
+            res_2 = self.aux_head1(res_2)
+            res_2 = F.softmax(res_2, dim=1)
+
+            res_3 = self.aux_head2(res_3)
+            res_3 = F.softmax(res_3, dim=1)
+
+            res_4 = self.aux_head3(res_4)
+            res_4 = F.softmax(res_4, dim=1)
+
+        if self.aux:
+            return [out, res_2, res_3, res_4]
         else:
-            return final_predict
+            return out
+
+# class Fusion(nn.Module):
+#     def __init__(self,nclasses, aux = True) -> None:
+#         super().__init__()
+#         self.num_classes = nclasses
+#         self.aux = aux
+#         self.backbone  = resnet_fpn_backbone('resnet50', weights='ResNet50_Weights.IMAGENET1K_V2', trainable_layers=5) 
+#         self.feature_extract = FeatureExtractionBlock()
+#         self.init_conv = BasicConv2d(5, 3, kernel_size=3, padding=1)
+#         self.feature_reduction_1 =BasicConv2d(256, 128, kernel_size=1, padding=0)
+#         self.feature_reduction_2 =BasicConv2d(128, 64, kernel_size=1, padding=0)
+#         self.predict_com =BasicConv2d(64*4, self.num_classes, kernel_size=1, padding=0)
+#         self.predict =BasicConv2d(64, self.num_classes, kernel_size=1, padding=0)
+
+#     def forward(self, x, rgb):
+#         x_feature = self.feature_extract(x)
+#         x = self.init_conv(x)
+#         x = self.backbone(x)
+#         x.pop("pool")
+#         x.pop("3")
+#         x = list(x.values())
+#         x = [x_feature] + x
+#         for i, feature in enumerate(x):
+#             x[i] = self.feature_reduction_2(self.feature_reduction_1(F.interpolate(feature, size=x_feature.size()[2:], mode='bilinear', align_corners=True)))
+
+#         final_predict = self.predict_com(torch.cat(x, dim=1))
+#         final_predict = F.softmax(final_predict, dim=1)
+#         if self.aux:
+#             predict_1 = F.softmax(self.predict(x[1]), dim=1)
+#             predict_2 = F.softmax(self.predict(x[2]), dim=1)
+#             predict_3 = F.softmax(self.predict(x[3]), dim=1)
+#             return [final_predict, predict_1, predict_2, predict_3]
+#         else:
+#             return final_predict
 
 
 
