fixing wrong order output of rangeprocess

common/laserscan.py
@@ -83,11 +83,11 @@ class LaserScan:
         # put in attribute
         points = scan[:, 0:3]  # get xyz
         remissions = scan[:, 3]  # get remission
-        if random.random() < self.aug_prob["point_dropping"]:
+        if random.random() < self.aug_prob["point_dropping"][0]:
             if self.aug_prob["scaling"] == 0.0:
-                self.drop_points = random.uniform(0.0, 0.5)
+                self.drop_points = random.uniform(0.0, self.aug_prob["point_dropping"][1])
             else:
-                self.drop_points = 0.1
+                self.drop_points = self.aug_prob["point_dropping"][0]
             self.points_to_drop = np.random.randint(0, len(points)-1,int(len(points)*self.drop_points))
             points = np.delete(points,self.points_to_drop,axis=0)
             remissions = np.delete(remissions,self.points_to_drop)

modules/network/RangePreprocess.py
@@ -14,7 +14,7 @@ class RangePreprocess():
     def __call__(self, data, mask, label, training=False):
         data = torch.cat([data, mask.unsqueeze(1)], dim=1)
         if not training:
-            return data, mask, label
+            return data, mask, label.long()
         data_list = list(torch.unbind(data))
         bs = len(data_list)
         out_scan = []
@@ -40,7 +40,7 @@ class RangePreprocess():
         out_scan = torch.stack(out_scan)
         out_label = torch.stack(out_label)
         out_mask = out_scan[:, -1, :, :]
-        return out_scan, out_label, out_mask
+        return out_scan, out_mask, out_label.long()
     
     def RangeUnion(self, scan_a, label_a, scan_b, label_b, k_union=0.5):
         scan_a_, label_a_ = scan_a.clone(), label_a.clone()