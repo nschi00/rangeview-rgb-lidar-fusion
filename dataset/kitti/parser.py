bdf425d45dcc65b2a7e52d6f02b46f70c7209501
nschi00 <131670912+nschi00@users.noreply.github.com>
2023-06-15 21:18:27 +0200

    Diasble skip and rotation around x augmentation.

--- dataset/kitti/parser.py
+++ dataset/kitti/parser.py
@@ -208,9 +208,9 @@ class SemanticKitti(Dataset):
             if random.random() > 0.5:
                 rot = True
                 # print("Rotation around z activated.")
-            if random.random() > 0.5:
-              rotx = np.random.normal(-10, 10, 1)[0]
-              rgb_data = TF.functional.rotate(rgb_data, rotx)
+            # if random.random() > 0.5:
+            #   rotx = np.random.normal(-10, 10, 1)[0]
+            #   rgb_data = TF.functional.rotate(rgb_data, rotx)
               # print("Rotation around x activated.")
             drop_points = random.uniform(0, 0.5)
 
